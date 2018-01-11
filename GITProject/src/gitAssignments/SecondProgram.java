5f7b510bc35078af21b743e5f4a19872a4e02ca2
Prokarma <Prokarma@Prokarma-PC>
2018-01-11 20:18:24 +0530

    commiting second program

---
+++ GITProject/src/gitAssignments/SecondProgram.java
@@ -0,0 +1,14 @@
+package gitAssignments;
+
+public class SecondProgram {
+public static void main(String[] args) {
+	int number1=10;
+	int number2=30;
+	int number3;
+	number3= number1+ number2;
+		System.out.println(number3);
+	
+}
+
+
+}
